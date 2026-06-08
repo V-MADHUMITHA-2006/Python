7a582bb001079b1a30abbcb10dfb34d9878d5d0d
Madhumitha V <madhumithaah06@gmail.com>
2026-06-08 21:37:37 +0530

    Add files via upload

---
+++ Array/Problems/Check_Arrays_Are_Same.py
@@ -0,0 +1,10 @@
+def check_arrays(arr1, arr2):
+    if arr1 == arr2:
+        print("Both arrays are the same.")
+    else:
+        print("Arrays are not the same.")
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [1, 2, 3, 4, 5]
+
+check_arrays(arr1, arr2)
